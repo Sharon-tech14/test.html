cc7882235eb8321dd2244179246f800b384660f7
Sharon-tech14 <ogbonnasharon20@gmail.com>
2024-07-22 21:05:35 +0100

    test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+
+<!-- saved from url=(0054)file:///C:/Users/USER/Downloads/test%20javascript.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><h1>HEY👀 !!!</h1>
+  
+    <style>
+      h1 {
+        color: blue;
+        text-align: center;
+        font-size: 100;
+      }
+    </style>
+    <script>
+      let name = prompt("What's your name?");
+      if (name === "Kris") {
+        alert("Hey kris hope you like it☺️");
+      }
+      if (name === "Tuayerin") {
+        alert("T boo ☺️");
+      }
+    </script>
+  
+
+</body></html>
